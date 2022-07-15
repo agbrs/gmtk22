6aef2443af55f2db320f06e0b79c276809a7ad5b
Gwilym Kuiper <gw@ilym.me>
2022-07-16 00:03:29 +0100

    Extract stuff to the battle module

--- src/battle.rs
+++ src/battle.rs
@@ -1,4 +1,67 @@
-use crate::{Agb, PlayerDice};
+use crate::{Agb, Face, PlayerDice};
+use agb::hash_map::HashMap;
+use alloc::vec::Vec;
+
+/// A face of the rolled die and it's cooldown (should it be a malfunction)
+#[derive(Debug)]
+
+struct RolledDie {
+    face: Face,
+    cooldown: u32,
+}
+
+impl RolledDie {
+    fn update(&mut self) {
+        self.cooldown = self.cooldown.wrapping_sub(1)
+    }
+
+    fn can_reroll(&self) -> bool {
+        self.face != Face::Malfunction || self.cooldown == 0
+    }
+}
+
+#[derive(Debug)]
+struct RolledDice {
+    rolls: Vec<RolledDie>,
+}
+
+impl RolledDice {
+    fn update(&mut self) {
+        self.rolls.iter_mut().for_each(RolledDie::update);
+    }
+}
+
+#[derive(Debug)]
+struct PlayerState {
+    shield_count: u32,
+    health: u32,
+}
+
+#[derive(Debug)]
+struct EnemyState {}
+
+#[derive(Debug)]
+struct CurrentBattleState {
+    player: PlayerState,
+    enemy: EnemyState,
+    rolled_dice: RolledDice,
+}
+
+impl CurrentBattleState {
+    fn accept_rolls(&mut self) {
+        let rolls = &self.rolled_dice.rolls;
+        let mut face_counts: HashMap<Face, u32> = HashMap::new();
+        for f in rolls {
+            *face_counts.entry(f.face).or_default() += 1;
+        }
+
+        // shield
+        let shield = face_counts.entry(Face::Shield).or_default();
+        if self.player.shield_count <= *shield {
+            self.player.shield_count += 1;
+        }
+    }
+}
 
 pub(crate) fn battle_screen(agb: &mut Agb, player_dice: PlayerDice) {
     loop {
--- src/main.rs
+++ src/main.rs
@@ -10,13 +10,12 @@
 // which won't be a particularly clear error message.
 #![no_main]
 
+use agb::display;
 use agb::display::object::{ObjectController, Sprite, Tag};
 use agb::display::tiled::VRamManager;
 use agb::display::Priority;
-use agb::hash_map::HashMap;
 use agb::interrupt::VBlank;
 use agb::rng::RandomNumberGenerator;
-use agb::{display, syscall};
 
 extern crate alloc;
 use alloc::vec::Vec;
@@ -75,67 +74,6 @@ impl Die {
 #[derive(Debug, Clone)]
 struct PlayerDice {
     dice: Vec<Die>,
-}
-
-/// A face of the rolled die and it's cooldown (should it be a malfunction)
-#[derive(Debug)]
-
-struct RolledDie {
-    face: Face,
-    cooldown: u32,
-}
-
-impl RolledDie {
-    fn update(&mut self) {
-        self.cooldown = self.cooldown.wrapping_sub(1)
-    }
-
-    fn can_reroll(&self) -> bool {
-        self.face != Face::Malfunction || self.cooldown == 0
-    }
-}
-
-#[derive(Debug)]
-struct RolledDice {
-    rolls: Vec<RolledDie>,
-}
-
-impl RolledDice {
-    fn update(&mut self) {
-        self.rolls.iter_mut().for_each(RolledDie::update);
-    }
-}
-
-#[derive(Debug)]
-struct PlayerState {
-    shield_count: u32,
-    health: u32,
-}
-
-#[derive(Debug)]
-struct EnemyState {}
-
-#[derive(Debug)]
-struct CurrentBattleState {
-    player: PlayerState,
-    enemy: EnemyState,
-    rolled_dice: RolledDice,
-}
-
-impl CurrentBattleState {
-    fn accept_rolls(&mut self) {
-        let rolls = &self.rolled_dice.rolls;
-        let mut face_counts: HashMap<Face, u32> = HashMap::new();
-        for f in rolls {
-            *face_counts.entry(f.face).or_default() += 1;
-        }
-
-        // shield
-        let shield = face_counts.entry(Face::Shield).or_default();
-        if self.player.shield_count <= *shield {
-            self.player.shield_count += 1;
-        }
-    }
 }
 
 struct Agb<'a> {
