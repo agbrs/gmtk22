c7618438048a2b08ad171e1a79bdffa1bb4dca0c
Corwin <corwin@kuiper.dev>
2022-07-15 20:21:41 +0100

    some structures for storing dice

--- src/main.rs
+++ src/main.rs
@@ -12,6 +12,30 @@
 
 use agb::{display, syscall};
 
+extern crate alloc;
+use alloc::vec::Vec;
+
+#[derive(Clone, Copy, PartialEq, Eq)]
+enum Face {
+    Attack,
+    Shield,
+    Malfunction,
+}
+
+struct Die {
+    faces: [Face; 6],
+}
+
+struct PlayerDice {
+    dice: Vec<Die>,
+}
+
+struct FaceIndex(usize);
+
+struct RolledDice {
+    rolls: Vec<FaceIndex>,
+}
+
 // The main function must take 1 arguments and never return. The agb::entry decorator
 // ensures that everything is in order. `agb` will call this after setting up the stack
 // and interrupt handlers correctly. It will also handle creating the `Gba` struct for you.
