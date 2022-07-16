7143505d521efb94981936b0dfb6962a4beb2171
Corwin <corwin@kuiper.dev>
2022-07-16 22:21:25 +0100

    dial down the power for everything

--- src/level_generation.rs
+++ src/level_generation.rs
@@ -8,7 +8,7 @@ pub struct GeneratedAttack {
 }
 
 pub fn generate_attack(current_level: u32) -> Option<GeneratedAttack> {
-    if (rng::gen().rem_euclid(128) as u32) < current_level * 2 {
+    if (rng::gen().rem_euclid(1024) as u32) < current_level * 2 {
         Some(GeneratedAttack {
             attack: generate_enemy_attack(current_level),
             cooldown: generate_cooldown(current_level),
@@ -18,15 +18,15 @@ pub fn generate_attack(current_level: u32) -> Option<GeneratedAttack> {
     }
 }
 
-fn generate_enemy_attack(_current_level: u32) -> EnemyAttack {
-    let attack_id = rng::gen().rem_euclid(8) as u32;
+fn generate_enemy_attack(current_level: u32) -> EnemyAttack {
+    let attack_id = rng::gen().rem_euclid(10) as u32;
 
-    if attack_id < 3 {
-        EnemyAttack::Shoot(rng::gen().rem_euclid(8) as u32 + 2)
-    } else if attack_id < 5 {
-        EnemyAttack::Shield(rng::gen().rem_euclid(3) as u32 + 1)
+    if attack_id < 4 {
+        EnemyAttack::Shoot(rng::gen().rem_euclid(((current_level + 2) / 3) as i32) as u32 + 1)
+    } else if attack_id < 9 {
+        EnemyAttack::Shield(rng::gen().rem_euclid(((current_level + 4) / 5) as i32) as u32 + 1)
     } else {
-        EnemyAttack::Heal(rng::gen().rem_euclid(8) as u32)
+        EnemyAttack::Heal(rng::gen().rem_euclid(((current_level + 1) / 2) as i32) as u32)
     }
 }
 
